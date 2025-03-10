483df690cd295dd3a3c7e8b4ec54390338b2cf4d
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2025-03-10 18:40:30 -0300

    wrappers

---
+++ ClassesUtilitarias/wrapper/WrapperTest01.java
@@ -0,0 +1,24 @@
+package ClassesUtilitarias.wrapper;
+
+public class WrapperTest01 {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10f;
+        double doubleP = 10d;
+        char charP = 'P';
+        boolean booleanP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10f;
+        Double doubleW = 10d;
+        Character charW = 'W';
+        Boolean booleanW = false;
+    }
+}
---
+++ ClassesUtilitarias/wrapper/WrapperTest02.java
@@ -0,0 +1,36 @@
+package ClassesUtilitarias.wrapper;
+
+public class WrapperTest02 {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10f;
+        double doubleP = 10d;
+        char charP = 'P';
+        boolean booleanP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1; // autoboxing
+        Long longW = 10L;
+        Float floatW = 10f;
+        Double doubleW = 10d;
+        Character charW = 'W';
+        Boolean booleanW = false;
+
+        int i = intW; // unboxing
+        Integer intW2 = Integer.parseInt("1");
+        boolean verdadeiro = Boolean.parseBoolean("TruE");
+
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetterOrDigit('9'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.toUpperCase('a'));
+        System.out.println(Character.toLowerCase('A'));
+    }
+}
