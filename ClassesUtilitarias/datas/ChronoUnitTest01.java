11c457d27cfbfbc2768b923f235aa79f95e7a940
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2025-03-25 17:56:09 -0300

    Instant | Period | Duration | ChronoUnit

---
+++ ClassesUtilitarias/datas/ChronoUnitTest01.java
@@ -0,0 +1,17 @@
+package ClassesUtilitarias.datas;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitTest01 {
+    public static void main(String[] args) {
+        LocalDateTime aniversario = LocalDateTime.of(2005, Month.JUNE, 25, 0, 0, 0);
+        LocalDateTime now = LocalDateTime.now();
+
+        System.out.println(ChronoUnit.DAYS.between(aniversario, now));
+        System.out.println(ChronoUnit.WEEKS.between(aniversario, now));
+        System.out.println(ChronoUnit.MONTHS.between(aniversario, now));
+        System.out.println(ChronoUnit.YEARS.between(aniversario, now));
+    }
+}
---
+++ ClassesUtilitarias/datas/InstantTest01.java
@@ -0,0 +1,18 @@
+package ClassesUtilitarias.datas;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public class InstantTest01 {
+    public static void main(String[] args) {
+        Instant now = Instant.now();
+        System.out.println(now);
+        System.out.println(LocalDateTime.now());
+        System.out.println(now.getEpochSecond());
+        System.out.println(now.getNano());
+        System.out.println(now.ofEpochSecond(3));
+        System.out.println(now.ofEpochSecond(3, 0));
+        System.out.println(now.ofEpochSecond(3, 1_000_000_000));
+        System.out.println(now.ofEpochSecond(3, -1_000_000_000));
+    }
+}
