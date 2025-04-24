ad1689828484bbd2959db243ac3d2c5679d54d24
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2025-04-24 16:11:11 -0300

    Generics | Wildcard

---
+++ Generics/GenericsTest01.java
@@ -0,0 +1,29 @@
+package Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Colecoes.map.Consumidor;
+
+public class GenericsTest01 {
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+        lista.add("Naruto");
+        lista.add("Goku");
+
+        for (String string : lista) {
+            System.out.println(string);
+        }
+
+        add(lista, new Consumidor("Eren"));
+
+        for (String string : lista) {
+            System.out.println(string);
+        }
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    private static void add(List lista, Consumidor consumidor) {
+        lista.add(consumidor);
+    }
+}
---
+++ Generics/WildcardTest01.java
@@ -0,0 +1,38 @@
+package Generics;
+
+abstract class Animal {
+    public abstract void consulta();
+}
+
+class Cachorro extends Animal {
+
+    @Override
+    public void consulta() {
+        System.out.println("Consultando Cachorro");
+    }
+
+}
+
+class Gato extends Animal {
+
+    @Override
+    public void consulta() {
+        System.out.println("Consultando Gato");
+    }
+
+}
+
+public class WildcardTest01 {
+    public static void main(String[] args) {
+        Cachorro[] cachorros = {new Cachorro(), new Cachorro()};
+        Gato[] gatos = {new Gato(), new Gato()};
+        printConsulta(cachorros);
+        printConsulta(gatos);
+    }
+
+    private static void printConsulta(Animal[] animals) {
+        for (Animal animal : animals) {
+            animal.consulta();
+        }
+    }
+}
