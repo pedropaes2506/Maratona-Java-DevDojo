02177390e55ef93d9af5fda1faee2d91f361e506
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2025-04-29 17:28:44 -0300

    Lambda | Predicate | Consumer | Function

---
+++ Lambdas/LambdaTest02.java
@@ -0,0 +1,22 @@
+package Lambdas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class LambdaTest02 {
+    public static void main(String[] args) {
+        List<String> strings = List.of("Pedro", "Naruto", "Goku", "Eren");
+        System.out.println(map(strings, s -> s.length()));
+        System.out.println(map(strings, s -> s.toUpperCase()));
+    }
+
+    private static <T, R> List<R> map(List<T> list, Function<T, R> function) {
+        List<R> result = new ArrayList<>();
+        for (T t : list) {
+            R r = function.apply(t);
+            result.add(r);
+        }
+        return result;
+    }
+}
