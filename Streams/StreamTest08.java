f2151a8a5e1a4d96eec45c98108ba57a7f62e710
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2025-05-19 17:21:45 -0300

    Reduce

---
+++ Streams/StreamTest08.java
@@ -0,0 +1,32 @@
+package Streams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamTest08 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+        new LightNovel("Tensei Shittara", 8.99),
+        new LightNovel("Overlord", 10.99),
+        new LightNovel("Violent Evergarden", 5.99),
+        new LightNovel("No Game no Life", 2.99),
+        new LightNovel("Fullmetal Alchemist", 5.99),
+        new LightNovel("Kumo Desuga", 1.99),
+        new LightNovel("Kumo Desuga", 1.99),
+        new LightNovel("Monogatari", 4.00)
+    ));
+
+    public static void main(String[] args) {
+        lightNovels.stream()
+        .map(LightNovel::getPrice)
+        .filter(price -> price > 3)
+        .reduce(Double::sum)
+        .ifPresent(System.out::println);
+
+        double sum = lightNovels.stream()
+        .mapToDouble(LightNovel::getPrice)
+        .filter(price -> price > 3)
+        .sum();
+
+        System.out.println(sum);
+    }
+}
