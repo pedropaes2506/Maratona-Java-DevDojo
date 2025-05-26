10f028f15e2289efc97deecec40c7a2ec4d029f4
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2025-05-26 17:06:56 -0300

    Priority | Sleep

---
+++ Threads/ThreadTest02.java
@@ -0,0 +1,43 @@
+package Threads;
+
+class ThreadExampleRunnable implements Runnable  {
+    private char c;
+
+    public ThreadExampleRunnable(char c) {
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        for (int i = 0; i < 500; i++) {
+            System.out.print(c);
+            if (i % 100 == 0) {
+                System.out.println();
+            }
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class ThreadTest02 {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new ThreadExampleRunnable('A'));
+        Thread t2 = new Thread(new ThreadExampleRunnable('B'));
+        Thread t3 = new Thread(new ThreadExampleRunnable('C'));
+        Thread t4 = new Thread(new ThreadExampleRunnable('D'));
+
+        t4.setPriority(Thread.MAX_PRIORITY);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        
+        System.out.println("#################" + Thread.currentThread().getName());
+    }
+}
